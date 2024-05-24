8293f66bfa56ca0fe0f4878e848f82577f5b2c29
kskashish023 <90233544+kskashish023@users.noreply.github.com>
2024-05-24 15:02:33 +0530

    Update App.java

--- App.java
+++ App.java
@@ -27,7 +27,17 @@ package com.iluwatar.fanout.fanin;
 import java.util.Arrays;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-
+/**
+ * FanOut/FanIn pattern is a concurrency pattern that refers to executing multiple instances of the
+ * activity function concurrently. The "fan out" part is essentially splitting the data into
+ * multiple chunks and then calling the activity function multiple times, passing the chunks.
+ *
+ * <p>When each chunk has been processed, the "fan in" takes place that aggregates results from each
+ * instance of function and forms a single final result.
+ *
+ * <p>This pattern is only really useful if you can “chunk” the workload in a meaningful way for
+ * splitting up to be processed in parallel.
+ */
   
 @Slf4j
 public class App {
